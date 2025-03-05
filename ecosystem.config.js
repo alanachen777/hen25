1f7c4a99c90dce3c13b88c7f8234cce8fb9feda0
alanachen777 <alana.chen777@gmail.com>
2025-03-05 10:30:09 -0500

    eco config for servers continuously

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: "app",
+        script: "C:/Users/sunar/OneDrive/Documentos/GitHub/hen25/server.js",
+        env: {
+          NODE_ENV: "development",
+        },
+        env_production: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
+  
+  
